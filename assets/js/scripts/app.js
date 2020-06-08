e12859fa05f3e37735c9a72c7749e890f01c145e
p0wen <Philly.e@hotmail.de>
2020-06-08 12:57:33 +0000

    Introducing first Timer prototype

--- assets/js/scripts/app.js
+++ assets/js/scripts/app.js
@@ -1,7 +1,7 @@
-// Accessing the Quotes - How to work with APIs based on this tuttorial https://www.taniarascia.com/how-to-use-the-javascript-fetch-api-to-get-json-data
-var quote;
-var quoteTextElem = document.querySelector('.quotetext');
-var quoteAuthorElem = document.querySelector('.author');
+// Accessing the Quotes - How to work with APIs based on this tutorial https://www.taniarascia.com/how-to-use-the-javascript-fetch-api-to-get-json-data
+let quote;
+let quoteTextElem = document.querySelector('.quotetext');
+let quoteAuthorElem = document.querySelector('.author');
 
 // Fetch JSON feed from type.fit
 fetch("https://type.fit/api/quotes")
@@ -13,3 +13,48 @@ fetch("https://type.fit/api/quotes")
     quoteTextElem.innerHTML = `"${quote.text}"`;
     quoteAuthorElem.innerHTML = quote.author;
 });
+
+
+// Building a Timer 
+/**
+ * Define the meditation Time
+ * Define the warmup time
+ * define the cool down time
+ * calculate total length 
+ * press play -> switch icon to pause & show reset button
+ * count down 3 seconds
+ * play start sound
+ * run through first timer
+ * play start sound
+ * start main timer
+ * play start sound
+ * after main timer ends start cool down timer
+ * play end bell
+ * display text "seize the day"
+ * store successfully finished session to local storage
+ */
+// Getting the HTML Element and declare meditation duration standard
+var meditationTimeElem = document.querySelector('.meditationtime') 
+var meditationDuration = 600;
+
+// Updating the Meditation Duration based on settings‚
+meditationTimeElem.textContent = `${Math.floor(meditationDuration / 60)}:${Math.floor(meditationDuration % 60)}`;
+var meditationTime = document.querySelectorAll('.meditationduration button');
+
+meditationTime.forEach(option => {
+    option.addEventListener("click", function() {
+        meditationDuration = this.getAttribute("data-time");
+        meditationTimeElem.innerHTML = `${Math.floor(meditationDuration /60)}:${Math.floor(meditationDuration % 60)}`;
+                console.log('Timer set');
+    });
+});
+
+let meditationtimer = setInterval(function () {
+    if (meditationDuration <= 0){
+        clearInterval(meditationtimer);
+        meditationTimeElem.innerHTML = `Seize the day!`
+    } else {
+        meditationTimeElem.innerHTML = `${Math.floor(meditationDuration /60)}:${Math.floor(meditationDuration % 60)}`
+    }
+    meditationDuration -= 1;
+}, 1000)
