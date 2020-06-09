54b6c324f0545e0ccac7ecea10874460e9fb545f
p0wen <Philly.e@hotmail.de>
2020-06-09 08:13:18 +0000

    Fixed issues identified by JSHint

--- assets/js/scripts/app.js
+++ assets/js/scripts/app.js
@@ -1,26 +1,25 @@
 // Accessing the Quotes - How to work with APIs based on this tutorial https://www.taniarascia.com/how-to-use-the-javascript-fetch-api-to-get-json-data
 let quote;
-let quoteTextElem = document.querySelector('.quotetext');
-let quoteAuthorElem = document.querySelector('.author');
+let quoteTextElem = document.querySelector(".quotetext");
+let quoteAuthorElem = document.querySelector(".author");
 
 // Fetch JSON feed from type.fit
 fetch("https://type.fit/api/quotes")
-.then(function(response) {
+  .then(function (response) {
     return response.json();
-})
-.then(function(data) {
-    quote = data[Math.floor(Math.random()*100) + 1];
+  })
+  .then(function (data) {
+    quote = data[Math.floor(Math.random() * 100) + 1];
     quoteTextElem.innerHTML = `"${quote.text}"`;
     quoteAuthorElem.innerHTML = quote.author;
-});
+  });
 
-
-// Building a Timer 
+// Building a Timer
 /**
  * Define the meditation Time
  * Define the warmup time
  * define the cool down time
- * calculate total length 
+ * calculate total length
  * press play -> switch icon to pause & show reset button
  * count down 3 seconds
  * play start sound
@@ -34,12 +33,11 @@ fetch("https://type.fit/api/quotes")
  * store successfully finished session to local storage
  */
 
-
-let meditationTimeElem = document.querySelector('.meditationtime') 
+let meditationTimeElem = document.querySelector(".meditationtime");
 let meditationDuration = 600;
 let meditationTimer;
-let medTimeSetBtn = document.querySelectorAll('.meditationduration button'); 
-let startStopButton = document.querySelector('.starttimer');
+let medTimeSetBtn = document.querySelectorAll(".meditationduration button");
+let startStopButton = document.querySelector(".starttimer");
 let startStopBtnIcon = startStopButton.querySelector("i");
 let timeLeft;
 
@@ -48,68 +46,77 @@ init();
 // Updating the Meditation Duration based on settings‚
 
 function init() {
-    timerSettings();
+  timerSettings();
+  controlTimer
 }
 
 // Timer Settings
 
-function timerSettings () {
-    meditationTimeElem.textContent = `${Math.floor(meditationDuration / 60)}:${Math.floor(meditationDuration % 60)}`;
-    medTimeSetBtn.forEach(option => {
-    option.addEventListener("click", function() {
-        removeActive(medTimeSetBtn);
-        meditationDuration = this.getAttribute("data-time");
-        meditationTimeElem.innerHTML = `${Math.floor(meditationDuration /60)}:${Math.floor(meditationDuration % 60)}`;
-        option.classList.add("active")
-    })
-})
+function timerSettings() {
+  meditationTimeElem.textContent = `${Math.floor(
+    meditationDuration / 60
+  )}:${Math.floor(meditationDuration % 60)}`;
+  medTimeSetBtn.forEach((option) => {
+    option.addEventListener("click", function () {
+      removeActive(medTimeSetBtn);
+      meditationDuration = this.getAttribute("data-time");
+      meditationTimeElem.innerHTML = `${Math.floor(
+        meditationDuration / 60
+      )}:${Math.floor(meditationDuration % 60)}`;
+      option.classList.add("active");
+    });
+  });
 }
 
 // Control Start Stop Button
 
 function controlTimer() {
-    let status = startStopButton.getAttribute("data-status");
-    startStopButton.addEventListener("click", function() {
-        if (status === "stopped") {
-            startTimer();
-            this.dataSet.status = "playing";
-            startStopBtnIcon.classList.remove("fa-play");
-            startStopBtnIcon.classList.add("fa-pause");
-        } else {
-            pauseTimer();
-            this.dataSet.status = "stopped";
-            startStopBtnIcon.classList.remove("fa-pause");
-            startStopBtnIcon.classList.add("fa-play");
-        }
-    })
+  let status = startStopButton.getAttribute("data-status");
+  startStopButton.addEventListener("click", function () {
+    if (status === "stopped") {
+      startTimer();
+      this.dataSet.status = "playing";
+      startStopBtnIcon.classList.remove("fa-play");
+      startStopBtnIcon.classList.add("fa-pause");
+    } else {
+      pauseTimer();
+      this.dataSet.status = "stopped";
+      startStopBtnIcon.classList.remove("fa-pause");
+      startStopBtnIcon.classList.add("fa-play");
+    }
+  });
 }
 
 // Reset Active State on none chosen Settings
 
 function removeActive(settingsTimer) {
-    settingsTimer.forEach(setting => {
-        setting.classList.remove("active");
-    });
+  settingsTimer.forEach((setting) => {
+    setting.classList.remove("active");
+  });
 }
 
 // Counting down the meditationduration (Solution based on https://stackoverflow.com/questions/31106189/create-a-simple-10-second-countdown)
 
 function startTimer() {
-    meditationtimer = setInterval(function () {
-    if (meditationDuration <= 0){
-        clearInterval(meditationtimer);
-        meditationTimeElem.innerHTML = `Seize the day!`
+  meditationTimer = setInterval(function () {
+    if (meditationDuration <= 0) {
+      clearInterval(meditationTimer);
+      meditationTimeElem.innerHTML = `Seize the day!`;
     } else {
-        meditationTimeElem.innerHTML = `${Math.floor(meditationDuration /60)}:${Math.floor(meditationDuration % 60)}`
+      meditationTimeElem.innerHTML = `${Math.floor(
+        meditationDuration / 60
+      )}:${Math.floor(meditationDuration % 60)}`;
     }
     meditationDuration -= 1;
-}, 1000)
+  }, 1000);
 }
 
 function pauseTimer() {
-    clearInterval(meditationTimer);
-    timeLeft = meditationDuration;
-    onTimeElem.innerHTML = `${Math.floor(timeLeft /60)}:${Math.floor(timeLeft % 60)}`
+  clearInterval(meditationTimer);
+  timeLeft = meditationDuration;
+  meditationTimeElem.innerHTML = `${Math.floor(timeLeft / 60)}:${Math.floor(
+    timeLeft % 60
+  )}`;
 }
 
 /** Build a Streak Recording for past 7 days
@@ -117,8 +124,7 @@ function pauseTimer() {
  * if yes
  * check date before yesterday
  * if yes check the day before yesterday
- * if no clear local storage 
+ * if no clear local storage
  * mark square with an "x"
  * if 7 days in a row State "You are on a roll" & clear for the next week
- * 
  */
